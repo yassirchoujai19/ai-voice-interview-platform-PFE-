8919538f7f2822a1c3cde2332ab761f5a496f648
Yassir Choujai <yassirchoujai@gmail.com>
2025-04-15 00:21:44 +0100

    Initial commit - AI Voice Interview Platform

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+const AuthLayout = ({children} : {children : ReactNode
+ 
+}) => {
+  return (
+    <div className="auth-layout">
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout
