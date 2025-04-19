9e2bc6b626fc7df94d524f8345f5e8624dabb4b0
Yassir Choujai <yassirchoujai@gmail.com>
2025-04-19 01:33:32 +0100

    ✨ Fix call status comparison and improve TSX structure

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,14 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+       <h3>Interview Generation</h3>
+
+       <Agent  userName="You" userId="user1" type="generate" />
+    </>
+  )
+}
+
+export default page
